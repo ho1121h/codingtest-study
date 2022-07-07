6791ec970cc8bd1014585cea64e7d30623f7a61b
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-07-07 17:53:35 +0900

    [BOJ]서로 다른 부분 문자열의 개수 / 실버 3/ 20분

--- ho1121h/7-7/[BOJ]서로 다른 부분문자열의개수/서로 다른 부분 문자열의 개수.py
+++ ho1121h/7-7/[BOJ]서로 다른 부분문자열의개수/서로 다른 부분 문자열의 개수.py
@@ -1,9 +1,10 @@
 S = input()
 A = set()
 
-for i in range(len(S)):
+for i in range(len(S)): #abcde 가 있으면 a,ab,abc,abcd,abcde출력
     for j in range(i,len(S)):
         N= S[i:j+1]
+        #print(N)
         A.add(N)
 #print(A) # 중복이 제거된채로 출력됨
 print(len(A))
