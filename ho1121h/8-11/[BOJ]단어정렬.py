1bc84320bffa0daf7ff1cd7cc4037a8a0bacba1b
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-08-11 19:04:27 +0900

    백준 /단어정렬 / 실버 5/ 20분

--- ho1121h/8-11/[BOJ]단어정렬.py
+++ ho1121h/8-11/[BOJ]단어정렬.py
@@ -10,6 +10,10 @@ for i in range(N):
     word.append(M)
 set(word)
 # sorted(word)
-word.sort(key= lambda x: (len(x),x))
+word.sort(key= lambda x: (len(x),x)) # 키설정
+new_word = []
 for i in word:
+    if i not in new_word: # 중복제거
+        new_word.append(i)
+for i in new_word:
     print(i)
