c6e624a3835443622a9e50539055e744b99fedc6
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-08-22 20:52:55 +0900

    [boj] 2문제/ 주석 추가

--- ho1121h/8-22/[BOJ]Yangjojang of The Year.py
+++ ho1121h/8-22/[BOJ]Yangjojang of The Year.py
@@ -1,14 +1,17 @@
-t = int(input())
+####
+# https://www.acmicpc.net/problem/11557
+####
+t = int(input()) # 전체 테스트 케이스 수
 
 for i in range(t):
-    a = int(input())
-    oh = []
+    a = int(input()) # 테스트 케이스수 입력
+    oh = [] # 
 
     for j in range(a):
 
-        b = input().split()
-        c = b[0]
-        d = b[1]
-        oh.append([b,int(d)])
-    oh.sort(key= lambda x: x[1])
-    print(oh[-1][0][0])
+        b = input().split() # Yonsei 10 -> ['Yonsei', '10']
+        c = b[0] # 학교이름
+        d = b[1] # 술 소비양
+        oh.append([b,int(d)]) #1차원 리스트로 추가
+    oh.sort(key= lambda x: x[1]) # 소비가 가장많은 학교의 이름 정렬 key는 술의 양으로 설정
+    print(oh[-1][0][0]) # 3차원이 생성됐으므로 이렇게 접근
--- ho1121h/8-22/[BOJ]평균은 넘겠지.py
+++ ho1121h/8-22/[BOJ]평균은 넘겠지.py
@@ -1,18 +1,21 @@
-import sys
-t = int(sys.stdin.readline())
-for i in range(t):
+# https://www.acmicpc.net/problem/4344
+
+import sys 
+input = sys.stdin.readline
+t = int(input())
+for i in range(t): 
      
-    a = sys.stdin.readline().split()
-    m= []
+    a = input().split() #입력
+    m= [] # 숫자 리스트
     for j in a:
         j =int(j)
         m.append(j)
-    b = m.pop(0)
-    avg = sum(m)/b
-    avg_l= []
+    b = m.pop(0) # 학생수 제거
+    avg = sum(m)/b # 평균 구함 
+    avg_l= []   # 평균을 넘는 학생 수
     for k in m:
-        if k > avg:
-            avg_l.append(k)
-    평균비율 = (len(avg_l)/b)*100
-    print(f'{평균비율:.03f}%')
+        if k > avg: # 평균이 넘으면
+            avg_l.append(k) # 추가
+    평균비율 = (len(avg_l)/b)*100 # 비율 계산
+    print(f'{평균비율:.03f}%') # 소숫점 3자리까지 
     # print(f'{round(len(avg_l)/b)}%')
