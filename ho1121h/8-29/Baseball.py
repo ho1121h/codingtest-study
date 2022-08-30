ebe9e6144b2f384029496fa440525db747f5c3d9
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-08-30 17:35:06 +0900

    Update Baseball.py

--- ho1121h/8-29/Baseball.py
+++ ho1121h/8-29/Baseball.py
@@ -9,9 +9,9 @@ for i in range(T):
     y =0
     k =0
     
-    for j in range(9):
-        a, b = map(int,input().split())
-        y +=a
+    for j in range(9): # 기본 9회 진행되므로 
+        a, b = map(int,input().split()) # 그냥 단순히 9번 입력받도록
+        y +=a # 점수는 간단하게 더하는 식으로
         k +=b
     if y < k :
         print("Korea")
