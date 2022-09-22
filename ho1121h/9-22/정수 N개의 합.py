6925ee21e3b3bc5ffc176e833d5e0dfc6f9a1d2f
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-09-22 18:57:29 +0900

    [BOJ] 정수N개의 합 / 브론즈2 / 20분

---
+++ ho1121h/9-22/정수 N개의 합.py
@@ -0,0 +1,16 @@
+# import sys
+
+# input = sys.stdin.readline
+
+
+
+# 입력은 백준에서 알아서 입력해주니 간단하게 sum 만쓰면된다.
+
+def solve(a):
+    # L = [i for i in range(a) ]
+    # M = sum(L)
+    # print(M)
+    return sum(a)
+
+# a = int(input())
+# solve(a)
