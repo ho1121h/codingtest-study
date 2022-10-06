b601e8c63909e4ec0e3d49e56e984dc8fd92868a
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-10-06 14:29:20 +0900

    boj/ 명령프롬프트/ 브론즈1/ 20분

---
+++ ho1121h/10-3/명령 프롬프트.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input()) 
+a = list(input()) # 첫번째 문자열을 비교대상으로 선정
+# print(a)
+a_len = len(a) # 인덱싱을 위한것
+
+for i in range(n - 1): 
+    b = list(input()) # 나머지 문자열 입력
+    for j in range(a_len): # 
+        if a[j] != b[j]:
+            a[j] = '?'
+# print(b)
+print(''.join(a))
