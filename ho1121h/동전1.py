f73b208e693fefa384f46c6fdada76f96db1c570
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-10-17 14:18:55 +0900

    Update 동전1.py

--- ho1121h/동전1.py
+++ ho1121h/동전1.py
@@ -17,6 +17,6 @@ dp[0] = 1 # 동전을 1개 쓸때 대비
 for i in coins : # 동전 하나씩 탐색
     for j in range(i, len(dp)): # 동전의 종류를 순회함
         if j - i >= 0 : # 특
-            dp[j] +=dp[j-i]
-
+            dp[j] += dp[j-i]
+            print(f'{i}원: {dp}')
 print(dp[k]) # 합이 k원이 되는 경우의 수를 출력
