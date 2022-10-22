fd347b18c4ded7da93cdcdde4e7da32b81db7883
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-10-22 20:05:12 +0900

    boj/달팽이는올라가고싶다/실버5

---
+++ ho1121h/달팽이는올라가고싶다.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+# A 낮에 A미터 올라감
+# B 밤에 B미터 내려감
+# V 높이가 V 미터임
+# 시간 제한 : 0.15 초
+A, B, V = map(int, input().split()) # ex : 2 1 5
+
+X = (V-B) / (A-B)
+# print(X)
+if X == int(X): # 만약 정수 = 딱맞아 떨어지면 출력
+    print(int(X))
+else:
+    print(int(X)+1) # 실수면 +1 해서 출력
+
+
+
+
+
